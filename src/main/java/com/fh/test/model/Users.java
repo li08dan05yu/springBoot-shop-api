6b5c13b881e07fc5a8eb6d280d48dc4edbb42cd3
lidanyu <1368796447@qq.com>
2020-08-04 12:01:48 +0800

    第二次上传

--- src/main/java/com/fh/test/model/Users.java
+++ src/main/java/com/fh/test/model/Users.java
@@ -1,5 +1,6 @@
 package com.fh.test.model;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -51,8 +52,24 @@ public class Users {
 
         Map<Users,String> map = new HashMap<>();
         Users user = new Users(11,"王二小");
-        map.put(user,"高薪就业");
+        Users user1 = new Users(12,"王二小");
+        Users user2 = new Users(13,"王二小");
+        Users user3 = new Users(14,"王二小");
+        map.put(user,"啊啊啊啊啊");
+        map.put(user1,"哈哈哈哈");
+        map.put(user2,"嘿嘿嘿嘿嘿");
+        map.put(user3,"呵呵呵呵呵");
+
+        Map<Users, String> usersStringMap = Collections.synchronizedMap(map);
 
         System.out.println(map.get(new Users(11,"王二小")));
+        System.out.println(map.get(user1));
+        System.out.println(map.get(user2));
+        System.out.println(map.get(user3));
+        System.out.println(usersStringMap.get(new Users(11,"王二小")));
+        System.out.println(usersStringMap.get(user1));
+        System.out.println(usersStringMap.get(user2));
+        System.out.println(usersStringMap.get(user3));
+
     }
 }
